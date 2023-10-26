3730e1227edaf2e402f7dcd938498e47d695f8c7
Javier <ibrahimcruz06@gmail.com>
2023-10-25 18:21:34 -0600

    Lista enlazada terminada con lista aleatoria y secuencial

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,43 @@ int generateRandomKey(){
 
 int main(){
 
-    llist<int> myList;  
+    llist<int> myList;//Lista para las llaves aleatorias 
+    llist<int> listaSecuencial; //Lista para las llaves secuenciales
 
     srand(static_cast<unsigned>(time(nullptr))); 
 
-    //Insertar los n nodos
     const int n = 1000000; 
+
+    //Insertar las llaves secuenciales en la lista secuencial 
+    for(int i = 0; i < n; i++){ 
+        llnode<int>* newNode = new llnode<int>(i); 
+        listaSecuencial.Insert(newNode); 
+    }
+
+
+    //Insertar los n nodos aleatorios
+    
     for(int i = 0; i < n; i++){ 
         int randomKey = generateRandomKey(); 
         llnode<int>* newNode = new llnode<int>(randomKey); 
         myList.Insert(newNode); 
     } 
 
-    //Medir el tiempo transcurrido en segundos 
+    //Medir el tiempo transcurrido en segundos para aleatoria
     auto start = std::chrono::steady_clock::now(); 
     auto end = std::chrono::steady_clock::now(); 
-    int busquedas = 0; 
+    int busquedasAleatorias = 0;  
 
+
+    //Busqueda en lista aleatoria
     while(std::chrono::duration_cast<std::chrono::seconds>(end - start).count() < 1){
         int randomSearchKey = generateRandomKey(); 
-        llnode<int>* result = myList.Search(randomSearchKey); 
-        busquedas++; 
+        llnode<int>* result = listaSecuencial.Search(randomSearchKey); //CAMBIAR AQUI EL TIPO DE LISTA PARA APUNTAR LOS RESULTADOS
+        busquedasAleatorias++; 
         end = std::chrono::steady_clock::now(); 
     } 
 
-    std::cout << "Se realizaron " << busquedas << " busquedas con valores aleatorios en un segundo." << std::endl; 
+    std::cout << "Se realizaron " << busquedasAleatorias << " busquedas en un segundo." << std::endl; 
 
     return 0; 
 }
